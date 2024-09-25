4f392a7747b2da110ff52acc8b6495cddc655d17
Stakan13 <mkbs1910@gmail.com>
2024-09-25 11:20:19 +0300

    save transport file

--- Transport.py
+++ Transport.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import xml.etree.ElementTree as ET
 
 
 # интерфейс предметной области
@@ -19,3 +20,45 @@ class Transport(ABC):
     @abstractmethod
     def to_xml(self, filename):
         pass
+
+
+class Car(Transport):
+
+    def __init__(self):
+        super().__init__()
+        self.name = input("Enter car name: ")
+
+        while True:
+            try:  # проверка правильности ввода данных
+                self.price = float(input("Enter price of transport:"))
+                if self.price <= 0:
+                    raise ValueError  # бросаем ошибку если что то не так
+                break
+            except ValueError:
+                print("Invalid input for price")  # сообщение об ошибке
+
+        self.movementType = input("Enter movement type of transport:")
+
+    def info(self):
+        print(f"Moving {self.name}(brrrrrr)")
+        print(f"Price of {self.name}: {self.price}")
+        print(f"Movement type is {self.movementType}")
+
+    def to_json(self, filename):  # метод для парсинга json в текстовый файл
+        return {
+            "type": "car",
+            "name": self.name,
+            "price": self.price,
+            "movementType": self.movementType
+        }
+
+    def to_xml(self, filename):  # метод для парсинго xml в текстовый файл
+        root = ET.Element("car")
+        name = ET.SubElement(root, "name")
+        name.text = self.name
+        price = ET.SubElement(root, "price")
+        price.text = str(self.price)
+        movement = ET.SubElement(root, "movementType")
+        movement.text = self.movementType
+
+        return root
