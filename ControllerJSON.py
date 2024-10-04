158e8cb08732e1f3a337e4ead19b2d9834d50131
Stakan13 <mkbs1910@gmail.com>
2024-10-05 01:25:26 +0300

    a big job

--- ControllerJSON.py
+++ ControllerJSON.py
@@ -2,19 +2,46 @@ import json
 
 
 class OperationJSON:
+
     @staticmethod
-    def safe_json(data, filename):
+    def safe_json(data, filename: str):
         try:
-            with open(filename, "w") as json_file:
-                json.dump(data, json_file, indent=4)
+            with open(filename, "w", encoding="utf-8") as json_file:
+                json.dump(data, json_file, indent=4, ensure_ascii=False)
             print("Saved file: " + filename)
         except IOError as e:
             print("error when starting json file: " + str(e))
 
     @staticmethod
-    def read_json(filename):
+    def load_json(filename):
         try:
-            with open(filename, "r") as json_file:
-                data = json.load(json_file)
+            with open(filename, "r", encoding="utf-8") as js:
+                result = json.load(js)
+                return result
         except FileNotFoundError:
-            print("That file not file")
+            return {"cars": [], "trucks": [], "buses": [], "planes": [],
+                    "helicopters": [], "submarines": []}
+
+    @staticmethod
+    def add_car(data, car):
+        data["cars"].append(car)
+
+    @staticmethod
+    def add_truck(data, truck):
+        data["trucks"].append(truck)
+
+    @staticmethod
+    def add_bus(data, bus):
+        data["buses"].append(bus)
+
+    @staticmethod
+    def add_plane(data, plane):
+        data["planes"].append(plane)
+
+    @staticmethod
+    def add_helicopter(data, helicopter):
+        data["helicopters"].append(helicopter)
+
+    @staticmethod
+    def add_submarine(data, submarine):
+        data["submarines"].append(submarine)
--- main.py
+++ main.py
@@ -1,70 +1,85 @@
 from Transport import Car, Truck, Bus, Helicopter, Plane, Submarine
+from ControllerJSON import OperationJSON
 
-def get_positive_num(messege: str) -> int:
+
+def get_positive_num(message: str) -> int:
     while True:
         try:
-            price = int(input(messege))
+            price = int(input(message))
             if price <= 0:
                 raise ValueError
             return price
         except ValueError:
             print("Please enter right value")
 
+
 def print_data():
     pass
 
+
 def main():
+
     print("choose operation \n"
           "1. print data from JSON\n"
-          "2. print data from XML\n"
-          "3. add car\n"
-          "4. add truck\n"
-          "5. add submarine\n"
-          "6. add bus\n"
-          "7. add helicopter\n"
-          "8. add plane\n"
-          "9. save to XML\n"
-          "10. save to JSON\n"
-          "11. exit")
+          "2. add car\n"
+          "3. add truck\n"
+          "4. add submarine\n"
+          "5. add bus\n"
+          "6. add helicopter\n"
+          "7. add plane\n"
+          "8. save to JSON\n"
+          "9. exit")
 
     while True:
         choice = int(input("Enter your choice: "))
 
-        if choice == 3:
+        if choice == 1:
+            pass
+
+        elif choice == 2:
             name = input("Enter car name: ")
             price = get_positive_num("Enter car price: ")
             movement_type = input("Enter car movement type: ")
             car = Car(name, price, movement_type)
+            OperationJSON.add_car(OperationJSON.load_json("result.json"), car)
 
-        elif choice == 4:
+        elif choice == 3:
             name = input("Enter truck name: ")
             price = get_positive_num("Enter truck price: ")
             load_capacity = get_positive_num("Enter truck load capacity: ")
             truck = Truck(name, price, load_capacity)
+            OperationJSON.add_car(OperationJSON.load_json("result.json"), truck)
 
-        elif choice == 5:
+        elif choice == 4:
             name = input("Enter submarine name: ")
             price = get_positive_num("Enter submarine price: ")
             max_depth = get_positive_num("Enter submarine max depth: ")
             submarine = Submarine(name, price, max_depth)
+            OperationJSON.add_car(OperationJSON.load_json("result.json"), submarine)
 
-        elif choice == 6:
+        elif choice == 5:
             name = input("Enter bus name: ")
             price = get_positive_num("Enter bus price: ")
             max_passengers = get_positive_num("Enter bus max passengers: ")
             bus = Bus(name, price, max_passengers)
+            OperationJSON.add_car(OperationJSON.load_json("result.json"), bus)
 
-        elif choice == 7:
+        elif choice == 6:
             name = input("Enter helicopter name: ")
             price = get_positive_num("Enter helicopter price: ")
             rpm = get_positive_num("Enter helicopter rpm: ")
             heli = Helicopter(name, price, rpm)
+            OperationJSON.add_car(OperationJSON.load_json("result.json"), heli)
 
-        elif choice == 8:
+        elif choice == 7:
             name = input("Enter plane name: ")
             price = get_positive_num("Enter plane price: ")
             speed = get_positive_num("Enter plane speed: ")
             plane = Plane(name, price, speed)
+            OperationJSON.add_car(OperationJSON.load_json("result.json"), plane)
+
+        elif choice == 8:
+
 
 
 if __name__ == "__main__":
