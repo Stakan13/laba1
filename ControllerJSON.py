097a6722b0a230979248a639d3432f48fdbbba93
Stakan13 <mkbs1910@gmail.com>
2024-10-07 23:00:30 +0300

    add comments

--- ControllerJSON.py
+++ ControllerJSON.py
@@ -6,22 +6,24 @@ class OperationJSON:
     @staticmethod
     def safe_json(data, filename: str):
         try:
-            with open(filename, "w", encoding="utf-8") as json_file:
+            with open(filename, "w", encoding="utf-8") as json_file:  # сохранение в файл
                 json.dump(data, json_file, indent=4, ensure_ascii=False, default=lambda o: o.__dict__)
             print("Saved file: " + filename)
-        except IOError as e:
+        except IOError as e:  # обработка ошибки при открытии фойла
             print("error when starting json file: " + str(e))
 
     @staticmethod
     def load_json(filename):
         try:
-            with open(filename, "r", encoding="utf-8") as js:
+            with open(filename, "r", encoding="utf-8") as js:  # чтение из json
                 result = json.load(js)
                 return result
-        except (FileNotFoundError, json.decoder.JSONDecodeError):
+        except (FileNotFoundError, json.decoder.JSONDecodeError):  # если файла нет или
+            # он пуст заполняем его пустым словарем
             return {"cars": [], "trucks": [], "buses": [], "planes": [],
                     "helicopters": [], "submarines": []}
 
+    # добавление инфы в словарь
     @staticmethod
     def add_car(data, car):
         data["cars"].append(car)
